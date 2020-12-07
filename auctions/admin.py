bee61a74f1bcd1d0d0b09c0df0aa665a2b03b18e
mlobacz <marcin.lobaczewski@gmail.com>
2020-12-07 22:58:12 +0100

    add models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Category, Listing, Comment, Bid, User
 
-# Register your models here.
+admin.site.register([Category, Listing, Comment, Bid, User])
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,60 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=16)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Listing(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="listings"
+    )
+    title = models.CharField(max_length=128)
+    date_created = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=11, decimal_places=2)
+    image_url = models.URLField(blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="listings"
+    )
+
+    def __str__(self):
+        return f'{self.id}: "{self.title}", created at {self.date_created}.'
+
+
+class Comment(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="comments"
+    )
+
+    def __str__(self):
+        return f"{self.id}: Comment by: {self.author}, created at {self.date_created}."
+
+
+class Bid(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=11, decimal_places=2)
+    bidder = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="bids"
+    )
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return f"{self.id}: Bid by: {self.bidder}, created at {self.date_created}."
 
 
 class User(AbstractUser):
-    pass
+    watching = models.ManyToManyField(Listing, blank=True, related_name="watched_by")
+
+    def __str__(self):
+        return f"{self.id}: {self.username}, {self.email}"
